c864e1cb666cf42fa569fceb2cbca032e346c9b4
rubenandiapimetel <rubenandiapimentel@gmail.com>
2023-04-14 18:24:46 +0200

    nothing imp

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+  import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import Modal from "react-modal";
 import { useState } from "react";
